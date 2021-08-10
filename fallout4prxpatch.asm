988422e5f5e1c3f37c7a93e7f5e2843039130857
Backporter <29647778+Backporter@users.noreply.github.com>
2021-08-10 03:06:36 -0400

    Update fallout4prxpatch.asm
    Added a 2nd patch that is more efficient and requires less bytes.

--- fallout4prxpatch.asm
+++ fallout4prxpatch.asm
@@ -1,3 +1,4 @@
+; 7/16/2021; Initial Patch 
 call    sceSystemServiceHideSplashScreen
 mov     eax, 1E38634h
 lea     edi, [eax]
@@ -6,4 +7,15 @@ mov     edx, 0
 mov     ecx, 0
 mov     eax, 0
 call    sceKernelLoadStartModule
-retn
+ret
+
+
+; 8/10/2021; 2nd Patch, better, and requires less bytes overall
+call    sceSystemServiceHideSplashScreen
+lea     edi, [0] ; replace 0 with a string
+xor     esi, esi
+xor     edx, edx
+xor     ecx, ecx
+xor     eax, eax
+call    sceKernelLoadStartModule
+ret
